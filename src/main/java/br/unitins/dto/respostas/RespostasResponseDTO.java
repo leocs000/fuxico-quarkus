17dea2890622354563dae1f0e7147ef89d3ffbcc
LEONARDO CARVALHO SILVA <leonardo.carvalho@uniggelsementes.com.br>
2025-06-02 17:45:05 -0300

    continuando crud avaliacao

--- src/main/java/br/unitins/dto/respostas/RespostasResponseDTO.java
+++ src/main/java/br/unitins/dto/respostas/RespostasResponseDTO.java
@@ -1,5 +1,19 @@
 package br.unitins.dto.respostas;
 
-public class RespostasResponseDTO {
+import br.unitins.dto.avaliacao.AvaliacaoResponseDTO;
+import br.unitins.dto.topico.TopicoResponseDTO;
+import br.unitins.model.Respostas;
 
-}
+public record RespostasResponseDTO(
+    Long id,
+    AvaliacaoResponseDTO avaliacao,
+    TopicoResponseDTO topico,
+    Double estrela
+) {
+    public static RespostasResponseDTO valueOf(Respostas respostas){
+        return new RespostasResponseDTO(
+            respostas.getId(), 
+            Avanull, null, null)
+    }
+} 
+
